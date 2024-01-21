a96df9c92a04b7f4478ad4d718f51b730fb9f023
Jake Deng <deng.jake@gmail.com>
2024-01-21 15:01:49 -0500

    1.1.1 - fix to retrieving secrets

--- utils/secrets.js
+++ utils/secrets.js
@@ -2,12 +2,16 @@ const { SSMClient, GetParameterCommand } = require('@aws-sdk/client-ssm');
 
 const ssmClient = new SSMClient({ region: 'us-east-1' });
 
-try {
-    const MONGODBURL = await ssmClient.send(new GetParameterCommand({ Name: '/bxcrd/prod/MONGODBURL', WithDecryption: true }));
-    const JWTSECRET = await ssmClient.send(new GetParameterCommand({ Name: '/bxcrd/prod/JWTSECRET', WithDecryption: true }));
-    exports.MONGODBURL = MONGODBURL;
-    exports.JWTSECRET = JWTSECRET;
-} catch (error) {
-    console.log(error);
-    throw error;
+async function fetchSecrets() {
+    try {
+        const MONGODBURL = await ssmClient.send(new GetParameterCommand({ Name: '/bxcrd/prod/MONGODBURL', WithDecryption: true }));
+        const JWTSECRET = await ssmClient.send(new GetParameterCommand({ Name: '/bxcrd/prod/JWTSECRET', WithDecryption: true }));
+        exports.MONGODBURL = MONGODBURL;
+        exports.JWTSECRET = JWTSECRET;
+    } catch (error) {
+        console.log(error);
+        throw error;
+    }
 }
+
+exports.fetchSecrets = fetchSecrets;
